e7f50a7f64ad3bb295186284bb275ba6bde9eef7
Maryyam18 <MaryyamFakhar39@gmail.com>
2024-02-29 17:57:16 +0500

    OOP

---
+++ Lab/Week 6/Task-1/BL/Authors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LAB_6
+{
+    internal class Authors
+    {
+        private string Name;
+        private string Email;
+        private string Gender;
+
+        public Authors(string Name, string Email, string Gender)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Gender = Gender;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetEmail(string Email)
+        {
+            this.Email = Email;
+        }
+        public string GetEmail()
+        {
+            return this.Email;
+        }
+        public void SetGender(string Gender)
+        {
+            this.Gender = Gender;
+        }
+        public string GetGender()
+        {
+            return this.Gender;
+        }
+    }
+}
---
+++ Lab/Week 6/Task-1/program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LAB_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Authors a = new Authors("Maryyam", "m39@gmail.com", "female");
+            Authors a1 = new Authors("Ayesha1", "a22@gmail.com", "female");
+            List<Authors> authorList = new List<Authors>() { a, a1 };
+            Books book = new Books("C#", 500F, authorList);
+
+            book.SetAuthorList(a1);
+            book.SetAuthorList(a);
+            book.SetBookName(book.GetBookName());
+
+            foreach(Authors author in authorList)
+            {
+                Console.WriteLine(book.GetBookName()+" "+author.GetName()+" "+author.GetEmail()+" "+author.GetGender());
+            }
+
+            Console.ReadLine();
+
+             
+
+        }
+    }
+}
